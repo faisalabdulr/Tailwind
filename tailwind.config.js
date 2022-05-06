c79202a0cb0f1e276fcaf91fd4e7fb57593c3389
andhiyanto <andhi1800018348@webmail.uad.ac.id>
2022-05-06 14:39:52 +0700

    portofolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding:'16px',
+    },
+
+    extend: {
+      colors: {
+        primary: '#10b981',
+        dark: '#1c1917',     
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
